a5e5e88b823927cf5d5d5656de85f45649f5ba42
amiteshu <amiteshu@cisco.com>
2022-04-28 18:41:26 +0530

    updating git sh file

--- demo/git_clone.sh
+++ demo/git_clone.sh
@@ -4,16 +4,16 @@ cd C:/My_space/Personal-cloud-repo/airflow-demo
 
 echo "_________Taking the latest pull from main___________"
 git pull origin main
-sleep 5
+sleep 3
 echo "_________Adding files to git___________"
 git add *
-sleep 4
+sleep 2
 echo "_________Checking status___________"
 git status
-sleep 4
+sleep 2
 echo "___________committing files to git_____________"
 git commit -m "Pushing from the shell script"
-sleep 4
+sleep 3
 echo "________pushing files to git_________"
 git push origin main
 sleep 4
