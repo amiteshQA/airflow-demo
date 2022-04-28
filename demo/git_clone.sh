632d998c4e932051b93d38ee89b2757c84af5e7e
amiteshu <amiteshu@cisco.com>
2022-04-28 21:14:02 +0530

    Pushing from the shell script

--- demo/git_clone.sh
+++ demo/git_clone.sh
@@ -5,19 +5,19 @@ cd C:/My_space/Personal-cloud-repo/airflow-demo
 git stash
 echo "_________Taking the latest pull from main___________"
 git pull origin main
-sleep 2
+#sleep 2
 git stash apply
-sleep 2
+#sleep 2
 echo "_________Adding files to git___________"
 git add *
-sleep 2
+#sleep 2
 echo "_________Checking status___________"
 git status
-sleep 2
+#sleep 2
 echo "___________committing files to git_____________"
 git commit -m "Pushing from the shell script"
-sleep 3
+#sleep 3
 echo "________pushing files to git_________"
 git push origin main
-sleep 2
+#sleep 2
 echo "________Files has been pushed_________"
