68af77748442bdb24627fee0dc2c6b66cc69d24c
amiteshu <amiteshu@cisco.com>
2022-04-28 11:06:26 +0530

    fixing the import error

--- dags/dag_populate_csv_.py
+++ dags/dag_populate_csv_.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import dagfactory
 
 
-dag_factory = dagfactory.DagFactory("/home/airflow/gcs/scripts/populate_csv_dag_config.yml")
+dag_factory = dagfactory.DagFactory("/home/airflow/gcs/dags/scripts/populate_csv_dag_config.yml")
 
 dag_factory.clean_dags(globals())
 dag_factory.generate_dags(globals())
